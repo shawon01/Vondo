f227b837417d1250a7fac8086a5e9bd30ac34415
shawon-sharma <dmission60@gmail.com>
2015-12-10 11:21:29 +0600

    initial commit

---
+++ app/src/androidTest/java/shawon/framgia/bd/com/vondo/ApplicationTest.java
@@ -0,0 +1,13 @@
+package shawon.framgia.bd.com.vondo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
